faba6cc561f388c91c8e3de49592c17b78c5be7e
prajaktajoshi2390 <70183176+prajaktajoshi2390@users.noreply.github.com>
2021-11-24 01:03:49 -0800

    edit rental dates functionality

--- src/Components/Cart/DeliverItems.js
+++ src/Components/Cart/DeliverItems.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import Grid from '@material-ui/core/Grid';
 import DeliveryAddresses from './DeliveryAddresses';
+import CartItem from './CartItem';
 
 const DeliverItems = (props) => {
   const [deliverItems, setDeliverItems] = React.useState([]);
@@ -12,55 +12,16 @@ const DeliverItems = (props) => {
     setDeliverItems(itemList)
   }, [props.cartDetails])
 
-  const formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-  }
   
   return (
     <div className="cartItemDetails">
       <DeliveryAddresses />
       {deliverItems.map((cartItem, index) => (
-        <Grid container className="cartItem">
-          <Grid item xs={4} sm={3}>
-            <img
-              src={cartItem.image}
-              alt="Product Details"
-              height="120"
-              width="120"
-              className="productImage"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6} className="cartItemDetails">
-            <h3>{cartItem.item_name}</h3>
-            <span>Purchase Type: {cartItem.purchaseType}</span>
-            <div className="cartShipment">
-              <span>Shipment:
-                {cartItem.deliveryOption === 0 ? ' Self Pickup' : ' Get it delivered'}
-              </span>
-              <span className="changeShipping" onClick={() => props.changeShipping(cartItem.item_id, 0)}>Change to Self Pickup</span>
-            </div>
-
-            <span>Shipping charges: $5</span>
-
-            {cartItem.purchaseType === 'Rent' ?
-              <span>Selected Dates: {formatDate(cartItem.rentStartDate)} to {formatDate(cartItem.rentEndDate)}</span>
-            : null}
-          </Grid>
-          <Grid item xs={2} sm={3} className="cartItemPrice">
-            <span>${cartItem.displayPrice}</span>
-            <span className="link" onClick={() => props.removeFromCart(cartItem)}>Remove</span>
-          </Grid>
-        </Grid>
+        <CartItem 
+          cartItem={cartItem}
+          removeFromCart = {(cartItem) => props.removeFromCart(cartItem)}
+          changeShipping = {(itemId, deliveryOption) => props.changeShipping(itemId, deliveryOption)}
+        />
       ))}
     </div>
   )
--- src/Components/Cart/PickUpItems.js
+++ src/Components/Cart/PickUpItems.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import Grid from '@material-ui/core/Grid';
-import { Link } from "react-router-dom";
+import CartItem from './CartItem';
 
 const PickUpItems = (props) => {
   const [pickupItems, setPickupItems] = React.useState([]);
@@ -12,62 +11,15 @@ const PickUpItems = (props) => {
     setPickupItems(itemList)
   }, [props.cartDetails])
 
-  const getpathQuery = (itemId) => {
-    const pathQuery = "/productDetails/" + itemId;
-    return {
-        pathname: pathQuery
-    };
-  };
-
-  const formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-  }
-  
   return (
     <div className="cartItemDetails">
       <h4>Pick Up</h4>
       {pickupItems.map((cartItem, index) => (
-        <Grid container className="cartItem">
-          <Grid item xs={4} sm={3}>
-            <img
-              src={cartItem.image}
-              alt="Product Details"
-              height="120"
-              width="120"
-              className="productImage"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6} className="cartItemDetails">
-            <Link to={getpathQuery(cartItem.item_id)} style={{ textDecoration: 'none', display: 'block', color:"inherit" }}>
-              <h3>{cartItem.item_name}</h3>
-            </Link>
-            <span>Purchase Type: {cartItem.purchaseType}</span>
-            <div className="cartShipment">
-              <span>Shipment:
-                {cartItem.deliveryOption === 0 ? ' Self Pickup' : ' Get it delivered'}
-              </span>
-              <span className="changeShipping" onClick={() => props.changeShipping(cartItem.item_id, 1)}>Get it delivered</span>
-            </div>
-            <span>Pickup Address: {cartItem.storeAddress}</span>
-            {cartItem.purchaseType === 'Rent' ?
-              <span>Selected Dates: {formatDate(cartItem.rentStartDate)} to {formatDate(cartItem.rentEndDate)}</span>
-            : null}
-          </Grid>
-          <Grid item xs={2} sm={3} className="cartItemPrice">
-            <span>${cartItem.displayPrice}</span>
-            <span className="link" onClick={() => props.removeFromCart(cartItem)}>Remove</span>
-          </Grid>
-        </Grid>
+        <CartItem 
+          cartItem={cartItem}
+          removeFromCart = {(cartItem) => props.removeFromCart(cartItem)}
+          changeShipping = {(itemId, deliveryOption) => props.changeShipping(itemId, deliveryOption)}
+        />
       ))}
     </div>
   )
